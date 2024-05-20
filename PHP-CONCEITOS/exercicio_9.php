da2c9483e6d0c4f1afc51471b9c6615b737fe8f6
Belicio Moreira <belicio.moreira@gmail.com>
2024-05-20 12:04:52 -0300

    ex09test

---
+++ PHP-CONCEITOS/exercicio_9.php
@@ -0,0 +1,14 @@
+<?php
+    $divisores = 0;
+    $contador = 1;
+
+    echo "Digite um número para verificar se é primo: ";
+    $numero = trim(fgets(STDIN));
+
+    if ($numero > 0) {
+        while ($contador ) {
+
+        }
+    }
+    
+?>
