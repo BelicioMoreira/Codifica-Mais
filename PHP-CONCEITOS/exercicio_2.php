e96d252b2dc7b977bae96f6569acdeb17607c160
Belicio Moreira <belicio.moreira@gmail.com>
2024-05-20 11:42:21 -0300

    exercicios 1 a 7

---
+++ PHP-CONCEITOS/exercicio_2.php
@@ -0,0 +1,14 @@
+<?php 
+    echo "Digite a 1° nota: ";
+    $nota1 = trim(fgets(STDIN));
+
+    echo "Digite a 2° nota: ";
+    $nota2 = trim(fgets(STDIN));
+
+    echo "Digite a 2° nota: ";
+    $nota3 = trim(fgets(STDIN));
+
+    $media = ($nota + $nota2 + $nota3) / 3;
+    
+    echo "A média das notas foram $media";
+?>
