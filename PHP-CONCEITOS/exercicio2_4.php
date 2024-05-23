8ce783a2d7a769a2e5b6fbe7fafc69cdab947231
Belicio Moreira <belicio.moreira@gmail.com>
2024-05-22 21:41:58 -0700

    exercicios-completos

--- PHP-CONCEITOS/exercicio2_4.php
+++ PHP-CONCEITOS/exercicio2_4.php
@@ -1,25 +1,25 @@
 <?php 
-    $num = 0;
+    echo "Digite um número (ou -1) para terminar: ";
+    $num = trim(fgets(STDIN));
     $maiorNum = $num;
     $menorNum = $num;
-   
 
     while ($num != -1) {
-        
+
         echo "Digite um número (ou -1) para terminar: ";
         $num = trim(fgets(STDIN));
-        
+
         if ($num > $maiorNum) {
             $maiorNum = $num;
         }
 
-        if ($menorNum < $maiorNum && $num != -1) {
+        if ($num < $menorNum && $num != -1) {
             $menorNum = $num;
         }
 
-        if ($num == -1 ) {
+        if ($num == -1 ) {  
             break;
-        }         
+        }
     }
      echo "O maior valor digitado foi: $maiorNum \n";
      echo "O menor valor digitado foi: $menorNum";
