0f65a78265812dc03be7a74c71d20af3a0953bab
Belicio Moreira <belicio.moreira@gmail.com>
2024-07-08 15:49:37 -0300

    ex-04-mod

--- SQL-MYSQL/ex-04.sql
+++ SQL-MYSQL/ex-04.sql
@@ -1,1 +1,23 @@
-ALTER TABLE produtos ADD CONSTRAINT fk_categoria FOREIGN KEY (categoria) REFERENCES categoria (id);
+USE gestao_produtos;
+
+ALTER TABLE produtos 
+DROP categoria,
+DROP unidade_medida,
+DROP fabricante, 
+DROP fornecedor;
+
+ALTER TABLE produtos
+ADD COLUMN categoria BIGINT,
+ADD CONSTRAINT fk_categoria FOREIGN KEY (categoria) REFERENCES categoria(id);
+
+ALTER TABLE produtos
+ADD COLUMN unidade_medida BIGINT,
+ADD CONSTRAINT fk_unidades_medida FOREIGN KEY (unidade_medida) REFERENCES unidades_medida(id);
+
+ALTER TABLE produtos
+ADD COLUMN fabricante BIGINT,
+ADD CONSTRAINT fk_fabricante FOREIGN KEY (fabricante) REFERENCES fabricantes(id);
+
+ALTER TABLE produtos
+ADD COLUMN fornecedor BIGINT,
+ADD CONSTRAINT fk_fornecedor FOREIGN KEY (fornecedor) REFERENCES fornecedores(id);
