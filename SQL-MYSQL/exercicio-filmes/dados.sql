1da5484054526dd4173d06a0fe663bd4a29153cf
BelicioMoreira <belicio.moreira@gmail.com>
2024-07-29 09:16:30 -0300

    ex-mysql-11-07 (#17)
    * ex-11-07
    
    * exercicios-completos

---
+++ SQL-MYSQL/exercicio-filmes/dados.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------
+-- Data for table `codifica`.`filmes`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `codifica`;
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Divertida Mente', 'Inside Out', 2015, 'Livre', 95, 'Animação, Aventura, Comédia', 8.1);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Duna: Parte 2', 'Dune: Part Two', 2024, '14', 166, 'Ação, Aventura, Drana', 8.6);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'O Senhor dos Anéis: A Sociedade do Anel', 'The Lord of the Rings: The Fellowship of the Ring', 2001, '12', 178, 'Aventura, Fantasia', 8.8);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'O Poderoso Chefão', 'The Godfather', 1972, '16', 175, 'Crime, Drama', 9.2);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Interestelar', 'Interstellar', 2014, '10', 169, 'Aventura, Drama, Sci-Fi', 8.6);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Clube da Luta', 'Fight Club', 1999, '18', 139, 'Drama', 8.8);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Matrix', 'The Matrix', 1999, '16', 136, 'Ação, Sci-Fi', 8.7);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Cidade de Deus', 'City of God', 2002, '18', 130, 'Crime, Drama', 8.6);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'Parque dos Dinossauros', 'Jurassic Park', 1993, 'Livre', 127, 'Aventura, Sci-Fi', 8.1);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'O Iluminado', 'The Shining', 1980, '16', 146, 'Drama, Horror', 8.4);
+INSERT INTO `codifica`.`filmes` (`id`, `titulo_do_filme`, `titulo_original`, `ano_de_lancamento`, `classificacao`, `duracao`, `categorias`, `avaliacao`) VALUES (DEFAULT, 'A Origem', 'Inception', 2010, '14', 148, 'Ação, Sci-Fi, Thriller', 8.8);
+
+COMMIT;
+
