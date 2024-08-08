272911bd018692fed786f7d5ebb5b3cdc9173c91
Belicio Moreira <belicio.moreira@gmail.com>
2024-08-07 23:28:55 -0300

    gerencia-estoque-v2.0

--- PHP-POO/GerenciaEstoque/Gerenciamento/Estoque.php
+++ PHP-POO/GerenciaEstoque/Gerenciamento/Estoque.php
@@ -4,10 +4,18 @@ namespace Gerenciamento;
 
 use Itens\Produto;
 use Itens\ProdutoPerecivel;
-
 class Estoque
 {
     private $estoque = [];
+
+    public function __construct()
+    {
+        $this->estoque[] = new Produto(101, 'NIK-AM-MAS-PR-42', 'Tênis Nike Air Max Masculino Preto Tamanho 42', 20, 'Unidade', 350.00);
+        $this->estoque[] = new Produto(102, 'CAD-ERG-AZ-UN', 'Cadeira de Escritório Ergônomica Azul', 15, 'Unidade', 450.00);
+        
+        $this->estoque[] = new ProdutoPerecivel(201, 'FRU-MAC-VER-UNI', 'Maçã Verde Unidade', 200, 'Unidade', 1.50, '10/09/2024');
+        $this->estoque[] = new ProdutoPerecivel(202, 'LAC-LEI-1L', 'Leite Integral 1 Litro', 150, 'Litro', 3.50, '15/09/2024');
+    }
 
     public function adicionarProduto(): void
     {
@@ -16,12 +24,12 @@ class Estoque
         $nome = readline("Digite o Nome do produto: ");
         $quantidade = readline("Digite a Quantidade: ");
         $unidadeMedida = readline("Digite a Unidade de medida: ");
-        $preco = readline("Digite o Preço do produto: ");
+        $preco = readline("Digite o Preço do produto: R$ ");
         $produto = new Produto($codigo, $sku, $nome, $quantidade, $unidadeMedida, $preco);
         $this->estoque[] = $produto;
-        echo "--------------------------\n";
-        echo "Produto adicionado.\n";
-        echo "--------------------------\n";
+        echo "-----------------------------------------\n";
+        echo "          Produto adicionado.\n";
+        echo "-----------------------------------------\n";
     }
 
     public function adicionarProdutoPerecivel(): void
@@ -35,44 +43,46 @@ class Estoque
         $dataValidade = readline("Digite a Data de Validade(dd/mm/aaaa): ");
         $produto = new ProdutoPerecivel($codigo, $sku, $nome, $quantidade, $unidadeMedida, $preco, $dataValidade);
         $this->estoque[] = $produto;
-        echo "--------------------------\n";
-        echo "Produto adicionado.\n";
-        echo "--------------------------\n";
+        echo "-----------------------------------------\n";
+        echo "          Produto adicionado.\n";
+        echo "-----------------------------------------\n";
     }
 
     public function venderProduto(): void
     {
         $codigoProcura = readline("Digite o código do produto: ");
 
-        foreach ($this->estoque as $produto) {
+        foreach ($this->estoque as $item => $produto) {
 
             if ($produto->getCodigo() == $codigoProcura) {
             
                 $quantidadeVenda = readline("Digite a quantidade para venda: ");
 
                 if ($quantidadeVenda > $produto->getQuantidade()) {
-                    echo "-------------------------------------------------------------------------\n";
+                    echo "-----------------------------------------\n";
                     echo "Existem apenas {$produto->getQuantidade()} deste produto\n";
-                    echo "-------------------------------------------------------------------------\n";
+                    echo "-----------------------------------------\n";
+
                 } else {
                     $produto->vendaProduto($quantidadeVenda);
-                    echo "-------------------------------------------------------------------------\n";
-                    echo "Foi vendido $quantidadeVenda unidades do produto {$produto->getNome()}.\n";
-                    echo "-------------------------------------------------------------------------\n";
-                }
+                    echo "-----------------------------------------\n";
+                    echo "Foi vendido $quantidadeVenda unidades do produto: \n{$produto->getNome()}.\n";
+                    echo "-----------------------------------------\n";
 
-                if ($produto->getQuantidade() == 0) {
-                    unset($this->estoque);
-                    echo "-------------------------------------------------------------------------\n";
-                    echo "O produto  {$produto->getNome()} foi removido, pois acabou o estoque. ". "\n";
-                    echo "-------------------------------------------------------------------------\n";
+                    if ($produto->getQuantidade() == 0) {
+                        unset($this->estoque[$item]);
+                        echo "-----------------------------------------\n";
+                        echo "O produto {$produto->getNome()} foi removido, pois acabou o estoque. ". "\n";
+                        echo "-----------------------------------------\n";
+                    }
+                    return;
                 }
             }
         }
 
-        echo "-------------------------------------------------------------------------\n";                
-        echo "Produto não encontrado.\n";                
-        echo "-------------------------------------------------------------------------\n";   
+        echo "-----------------------------------------\n";                
+        echo "          Produto não encontrado.\n";                
+        echo "-----------------------------------------\n";   
     }
 
     public function verificarEstoque(): void
@@ -81,29 +91,29 @@ class Estoque
 
         foreach ($this->estoque as $produto) {
             if ($produto->getCodigo() == $codigoProcura) {
-                echo "-------------------------------------------------------------------------\n";
-                echo "O produto {$produto->getNome()} está disponivel com {$produto->getQuantidade()} unidades\n";
-                echo "-------------------------------------------------------------------------\n";
+                echo "-----------------------------------------\n";
+                echo "Produto: \n{$produto->getNome()} \nestá disponivel com {$produto->getQuantidade()} unidades\n";
+                echo "-----------------------------------------\n";
                 return;
             }
         }
 
-        echo "-------------------------------------------------------------------------\n";                
-        echo "Produto não encontrado.\n";                
-        echo "-------------------------------------------------------------------------\n";   
+        echo "-----------------------------------------\n";                
+        echo "          Produto não encontrado.\n";                
+        echo "-----------------------------------------\n";  
     }
 
     public function listarEstoque(): void
     {
         if (empty($this->estoque)) {
-            echo "Estoque vazio.\n";
-            echo "-------------------------------------------------------------------------\n";
+            echo "            Estoque vazio.\n";
+            echo "-----------------------------------------\n";
             return;
         } 
         
         foreach ($this->estoque as $produto) {
-            echo "Código: {$produto->getCodigo()} SKU: {$produto->getSKU()} Nome: {$produto->getNome()} Quantidade: {$produto->getQuantidade()} Unidade de Medida: {$produto->getUnidadeMedida()} Preço: {$produto->getPreco()} \n";
-            echo "-------------------------------------------------------------------------\n";
+            echo "Código: {$produto->getCodigo()} \nSKU: {$produto->getSKU()} \nNome: {$produto->getNome()} \nQuantidade: {$produto->getQuantidade()} \nUnidade de Medida: {$produto->getUnidadeMedida()} \nPreço: {$produto->getPreco()} \n";
+            echo "-----------------------------------------\n";
         }
     }
 }
--- PHP-POO/GerenciaEstoque/Gerenciamento/Menu.php
+++ PHP-POO/GerenciaEstoque/Gerenciamento/Menu.php
@@ -32,44 +32,44 @@ class Menu
             
             switch ($opcao) {
                 case 1:
-                    echo "--------------------------\n";
-                    echo "Adicionar um produto\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "          Adicionar um Produto\n";
+                    echo "-----------------------------------------\n";
                     $estoque->adicionarProduto();
                     break;
                 case 2:
-                    echo "--------------------------\n";
-                    echo "Adicionar um produto\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "      Adicionar um Produto Perecível\n";
+                    echo "-----------------------------------------\n";
                     $estoque->adicionarProdutoPerecivel();
                     break;
                 case 3:
-                    echo "--------------------------\n";
-                    echo "Vender um produto\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "          Vender um Produto\n";
+                    echo "-----------------------------------------\n";
                     $estoque->venderProduto();
                     break;
                 case 4:
-                    echo "--------------------------\n";
-                    echo "Verificar Estoque\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "          Verificar Estoque\n";
+                    echo "-----------------------------------------\n";
                     $estoque->verificarEstoque();
                     break;
                 case 5:
-                    echo "--------------------------\n";
-                    echo "Listar o Estoque\n";
-                    echo "-------------------------------------------------------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "           Listar o Estoque\n";
+                    echo "-----------------------------------------\n";
                     $estoque->listarEstoque();
                     break;
                 case 6:
-                    echo "--------------------------\n";
-                    echo "Saindo...\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
+                    echo "                Saindo...\n";
+                    echo "-----------------------------------------\n";
                     exit;
                 default:
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
                     echo "Opção inválida, por favor tente novamente.\n";
-                    echo "--------------------------\n";
+                    echo "-----------------------------------------\n";
                     break;
             }
         }
