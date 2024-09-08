df98e8ba0f026ceccb3a20b5d55c86d0668e84c1
Belicio Moreira <belicio.moreira@gmail.com>
2024-09-08 13:46:29 -0300

    Atualização na listagem para exibir as cores da categoria

--- PROJETO/PROJETO/CRUD-WEB/src/View/Produto/listagem.php
+++ PROJETO/PROJETO/CRUD-WEB/src/View/Produto/listagem.php
@@ -33,22 +33,53 @@
         <?php if (empty($_SESSION['produtos'])) {?>
         <div>
             <p>Nenhum Produto Encontrado</p>
-        </div>
-           
+        </div>            
             <?php } else { ?>
-                <?php foreach ($_SESSION['produtos'] as $produto) {?>
-                    <div class="secao__itens vestuario">
+                <?php foreach ($_SESSION['produtos'] as $produto): ?>
+                    <?php
+                            switch ($produto['categoria_id']) {
+                                case 1:
+                                    $categoria = "categoria__eletronicos";
+                                    $categoriaNome = "Eletrônicos";
+                                    break;
+                                case 2:
+                                    $categoria = "categoria__eletrodomesticos";
+                                    $categoriaNome = "Eletrodomésticos";
+                                    break;
+                                case 3:
+                                    $categoria = "categoria__moveis";
+                                    $categoriaNome = "Móveis";
+                                    break;
+                                case 4:
+                                    $categoria = "categoria__decoracao";
+                                    $categoriaNome = "Decoração";
+                                    break;
+                                case 5:
+                                    $categoria = "categoria__vestuario";
+                                    $categoriaNome = "Vestuário";
+                                    break;
+                                case 7:
+                                    $categoria = "categoria__outros";
+                                    $categoriaNome = "Outros";
+                                    break;
+                                default:
+                                    $categoria = "categoria__outros"; // Categoria padrão
+                                    $categoriaNome = "Outros";
+                            }
+                    ?>
+
+                    <div class="secao__itens vestuario;">
                     <div class="secao__itens__item">
 
                         <div class="secao__itens__dados">
                             <div class="id__categoria__sku">
-                                <p class="id">#00000<?php echo $produto['id'];?></p>
-                                <p class="categoria <?php echo $categoria;?>"><?php echo $categoriaNome;?></p>
-                                <p class="sku"><?php echo $produto['sku'];?></p>
+                                <p class="id">#00000<?= $produto['id'] ;?></p>
+                                <p class="categoria <?= $categoria;?>"><?= $categoriaNome; ?></p>
+                                <p class="sku"><?= $produto['sku'];?></p>
                             </div>
                             <div class="nome__quantidade">
-                                <p class="nome"><?php echo $produto['nome'];?></p>
-                                <p class="quantidade"><?php echo $produto['quantidade'];?></p>
+                                <p class="nome"><?= $produto['nome'];?></p>
+                                <p class="quantidade"><?= $produto['quantidade'];?></p>
                             </div>
                         </div>
 
@@ -65,7 +96,7 @@
 
                     </div>
                 </div>
-            <?php }?>
+            <?php endforeach ?>
         <?php }?>
 
         </div>    
